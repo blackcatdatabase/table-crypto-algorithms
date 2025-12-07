bf921e4fbb3d0043fb7641daf613bfb7dfcb49d0
JaineBlack <jain.black@protonmail.com>
2025-12-07 01:26:07 +0100

    chore: regenerate src/schema after cleanup

--- schema/modules/blackcat-database-crypto/040_views_modules.postgres.sql
+++ schema/modules/blackcat-database-crypto/040_views_modules.postgres.sql
@@ -1,22 +1,5 @@
 -- Auto-generated from feature-modules-postgres.yaml (map@sha1:A8D58997CBCD2EEE06670B1C02AD89FA65E66F67)
 -- engine: postgres
--- table:  crypto_algorithms_deprecation
-
--- Algorithms nearing deprecation (status active but deprecation_date set)
-CREATE OR REPLACE VIEW vw_crypto_algorithms_deprecation AS
-SELECT
-  id,
-  name,
-  class,
-  status,
-  nist_level,
-  created_at
-FROM crypto_algorithms
-WHERE status = 'deprecated'
-ORDER BY created_at ASC;
-
--- Auto-generated from feature-modules-postgres.yaml (map@sha1:A8D58997CBCD2EEE06670B1C02AD89FA65E66F67)
--- engine: postgres
 -- table:  key_wrapper_layers_pq_readiness_summary
 
 -- One-row PQ readiness snapshot
@@ -33,4 +16,3 @@ SELECT
      FROM signatures s
      JOIN crypto_algorithms ca ON ca.id = s.algo_id
     WHERE ca.class='sig' AND ca.nist_level IS NOT NULL) AS pq_signatures_total;
-
